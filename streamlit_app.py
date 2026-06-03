bd12ecc02138c154b467e40d2067326c15295af4
bluesctr <mccrafterbluesctr@gmail.com>
2026-06-03 07:25:13 +0000

    c

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,40 +2,61 @@ import streamlit as st
 import base64
 from openai import OpenAI
 
-
+# API KEY 저장
 if "api_key" not in st.session_state:
     st.session_state.api_key = ""
 
-def get_llm_response(prompt):
-    response = client.responses.create(
-        model="gpt-5.4-mini",
-        input=prompt
-    )
-    return response.output_text
-
-def generate_image(image_prompt):
-    img = client.images.generate(model="gpt-image-1-mini", prompt=image_prompt)
-    image_bytes = base64.b64decode(img.data[0].b64_json)
-    return image_bytes
-
-st.title("OpenAI GPT model")
-
-api_key= st.text_input("OpenAI API Key", type="password", value=st.session_state.api_key)
+api_key = st.text_input(
+    "OpenAI API Key",
+    type="password",
+    value=st.session_state.api_key
+)
 
 st.session_state.api_key = api_key
 
 if api_key:
     client = OpenAI(api_key=api_key)
 else:
-    st.markdown("API KEY를 입력하세요.")
+    st.stop()
+
+st.title("OpenAI GPT model")
+
+
+# 캐시 적용
+@st.cache_data
+def get_llm_response(prompt, api_key):
+    client = OpenAI(api_key=api_key)
+
+    response = client.responses.create(
+        model="gpt-5.4-mini",
+        input=prompt
+    )
+
+    return response.output_text
+
+
+# 캐시 적용
+@st.cache_data
+def generate_image(image_prompt, api_key):
+    client = OpenAI(api_key=api_key)
+
+    img = client.images.generate(
+        model="gpt-image-1-mini",
+        prompt=image_prompt
+    )
+
+    image_bytes = base64.b64decode(img.data[0].b64_json)
+
+    return image_bytes
+
 
 prompt = st.text_area("User prompt")
 
-if st.button("Ask!", disabled=(len(prompt)==0)):
-    st.write(get_llm_response(prompt))
+if st.button("Ask!", disabled=(len(prompt) == 0)):
+    st.write(get_llm_response(prompt, api_key))
 
 
 image_prompt = st.text_area("Image prompt")
 
-if st.button("Generate!", disabled=(len(image_prompt)==0)):
-    st.image(generate_image(image_prompt))
+if st.button("Generate!", disabled=(len(image_prompt) == 0)):
+    st.image(generate_image(image_prompt, api_key))
